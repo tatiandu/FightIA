fd14ef91ad02c0b4a436cf38c6ced7e76fb78396
Tatiana <tatiandu@ucm.es>
2021-06-08 17:37:09 +0200

    Proyecto creado
    Co-Authored-By: Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>

---
+++ FightIA/Assets/Scripts/BarraVida.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraVida : MonoBehaviour
+{
+    private Slider barra;
+    private float valorFinal = 1;
+
+    public float velocidad = 0.5f;
+    public ParticleSystem particulas;
+    public Image imagenBarra;
+
+    private void Awake()
+    {
+        barra = gameObject.GetComponent<Slider>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (barra.value > valorFinal)
+        {
+            barra.value -= velocidad * Time.deltaTime;
+            if (!particulas.isPlaying)
+            {
+                particulas.Play();
+            }
+        }
+        else
+        {
+            particulas.Stop();
+        }
+
+        if(barra.value <= 0)
+        {
+            imagenBarra.enabled = false;
+            particulas.Stop();
+        }
+    }
+
+    public void PerderVida(float valor)
+    {
+        valorFinal = barra.value - valor;
+    }
+}
