1f03ca2115d69ee7d47607dd89b1504e353511e7
Tatiana <tatiandu@ucm.es>
2021-06-08 18:26:20 +0200

    PlayerController
    Co-Authored-By: Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>

---
+++ FightIA/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad;
+    public float fuerzaSalto;
+
+    Rigidbody rb;
+    bool enElSuelo;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //Salto
+        if (Input.GetKeyDown(KeyCode.Space) && enElSuelo)
+        {
+            rb.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Movimiento en horizontal del personaje
+        rb.velocity = new Vector3(velocidad * Input.GetAxis("Horizontal"), rb.velocity.y, rb.velocity.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Plane")
+        {
+            enElSuelo = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Plane")
+        {
+            enElSuelo = false;
+        }
+    }
+}
