fe55858e0b59469f79f4be3795bfb402f62ea23d
Tatiana <tatiandu@ucm.es>
2021-06-09 18:42:43 +0200

    Avances en los ataques y movimiento
    Co-Authored-By: Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>

--- FightIA/Assets/Scripts/PlayerController.cs
+++ FightIA/Assets/Scripts/PlayerController.cs
@@ -6,29 +6,127 @@ public class PlayerController : MonoBehaviour
 {
     public float velocidad;
     public float fuerzaSalto;
+    public float tiempoAtaque;
+    public GameObject[] hitboxAtaque; //0: Arriba, 1: Centro, 2: Abajo
+    public Collider[] hitboxJugador; //0: De pie, 1: Agachado
 
     Rigidbody rb;
-    bool enElSuelo;
+    bool enElSuelo; //Si esta en contacto con el suelo
+    bool atacando; //Si esta atacando
+    bool protegido; //Si se esta protegiendo
+    float temporizador; //Para el ataque
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        atacando = false;
+        protegido = false;
+        temporizador = 0;
     }
 
     void Update()
     {
-        //Salto
-        if (Input.GetKeyDown(KeyCode.Space) && enElSuelo)
+        Salto(); //Input de salto
+        Agachar(); //Input de agacharse
+        Proteger(); //Input de protegerse
+        Ataque(); //Input de ataque
+
+        if (atacando) //Si esta ejecutando un ataque
         {
-            rb.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode.Impulse);
+            temporizador += Time.deltaTime;
+
+            if (temporizador >= tiempoAtaque) { //Si ha pasado el tiempo de ataque
+                atacando = false;
+                temporizador = 0;
+
+                foreach (GameObject aux in hitboxAtaque) { //Desactivar hitbox activas
+                    aux.SetActive(false);
+                }
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        //Movimiento en horizontal del personaje
-        rb.velocity = new Vector3(velocidad * Input.GetAxis("Horizontal"), rb.velocity.y, rb.velocity.z);
+        //Movimiento en horizontal del personaje si no se esta protegiendo
+        if (!protegido)
+        {
+            rb.velocity = new Vector3(velocidad * Input.GetAxis("Horizontal"), rb.velocity.y, rb.velocity.z);
+
+            //Animacion correspondiente TODO
+        }
     }
+
+    void Salto()
+    {
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && enElSuelo && !protegido)
+        {
+            rb.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode.Impulse);
+        }
+
+        //Animacion correspondiente TODO
+    }
+
+    void Agachar()
+    {
+        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && enElSuelo)
+        {
+            hitboxJugador[0].enabled = false;
+            hitboxJugador[1].enabled = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            hitboxJugador[0].enabled = true;
+            hitboxJugador[1].enabled = false;
+        }
+
+        //Animacion correspondiente TODO
+    }
+
+    void Proteger()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && !atacando && enElSuelo)
+        {
+            protegido = true;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.I))
+        {
+            protegido = false;
+
+        }
+
+        //Animacion correspondiente TODO
+    }
+
+    void Ataque()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && !atacando) //Arriba
+        {
+            hitboxAtaque[0].SetActive(true); //Activar la hitbox del ataque
+            atacando = true; //Activar timer de duracion del ataque
+
+            //Animacion correspondiente TODO
+        }
+        else if (Input.GetKeyDown(KeyCode.K) && !atacando) //Centro
+        {
+            hitboxAtaque[1].SetActive(true); //Activar la hitbox del ataque
+            atacando = true; //Activar timer de duracion del ataque
+
+            //Animacion correspondiente TODO
+        }
+        else if (Input.GetKeyDown(KeyCode.L) && !atacando) //Abajo
+        {
+            hitboxAtaque[2].SetActive(true); //Activar la hitbox del ataque
+            atacando = true; //Activar timer de duracion del ataque
+
+            //Animacion correspondiente TODO
+        }
+    }
+
+
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
